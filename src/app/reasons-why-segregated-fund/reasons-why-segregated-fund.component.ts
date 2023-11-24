e301ad1d970af02de22eec62173b15ca6c0b8c86
JohnUz1981 <151864441+JohnUz1981@users.noreply.github.com>
2023-11-24 00:18:41 -0500

    11-24-2023-001

--- src/app/reasons-why-segregated-fund/reasons-why-segregated-fund.component.ts
+++ src/app/reasons-why-segregated-fund/reasons-why-segregated-fund.component.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatNativeDateModule, } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import * as jsPDF from 'jspdf';
+
 
 interface Advisor {
+  value: string;
+  viewValue: string;
+}
+interface Carrier {
   value: string;
   viewValue: string;
 }
@@ -16,7 +23,19 @@ interface Advisor {
 @Component({
   selector: 'app-reasons-why-segregated-fund',
   standalone: true,
-  imports: [CommonModule, RouterModule, FormsModule, MatFormFieldModule, MatInputModule, MatNativeDateModule, MatDatepickerModule, MatDatepickerModule, MatSelectModule],
+  imports: [
+    CommonModule,
+    RouterModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatDatepickerModule,
+    MatDatepickerModule,
+    MatSelectModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+  ],
   templateUrl: './reasons-why-segregated-fund.component.html',
   styleUrl: './reasons-why-segregated-fund.component.css'
 })
@@ -27,4 +46,14 @@ export class ReasonsWhySegregatedFundComponent {
     { value: 'Gerald Finnerty', viewValue: 'Gerald Finnerty' },
   ];
 
+  carrier: Carrier[] = [
+    { value: 'Canada Life', viewValue: 'Canada Life' },
+    { value: 'Manulife', viewValue: 'Manulife' },
+    { value: 'Sunlife', viewValue: 'Sunlife' },
+    { value: 'Equitable Life', viewValue: 'Equitable Life' },
+    { value: 'BMO', viewValue: 'BMO' },
+  ];
+  currentDate = new Date();
+  currentDateTime = new FormControl(this.currentDate);
+  currencyControl = new FormControl('', [Validators.required]);
 }
