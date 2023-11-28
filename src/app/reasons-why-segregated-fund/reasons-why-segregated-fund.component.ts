06c3d7b65e044d3c6c2da0242d9f04723dc80483
JohnUz1981 <151864441+JohnUz1981@users.noreply.github.com>
2023-11-27 21:51:34 -0500

    11-27-2025-002

--- src/app/reasons-why-segregated-fund/reasons-why-segregated-fund.component.ts
+++ src/app/reasons-why-segregated-fund/reasons-why-segregated-fund.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, JsonPipe } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatNativeDateModule } from '@angular/material/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatSelectModule } from '@angular/material/select';
@@ -11,9 +11,9 @@ import { MatButtonModule } from '@angular/material/button';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import * as jsPDF from 'jspdf';
 import { ServicesService } from '../services/services.service';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatCardModule} from '@angular/material/card';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatCardModule } from '@angular/material/card';
 
 interface Advisor {
   value: string;
@@ -28,7 +28,12 @@ interface Carrier {
 interface User {
   name: string;
 }
-
+interface SalesCharge {
+  salescharge: string;
+}
+interface Amounts {
+  amount: string;
+}
 
 @Component({
   providers: [ServicesService],
@@ -47,11 +52,14 @@ interface User {
     ReactiveFormsModule,
     MatButtonModule,
     HttpClientModule,
-    MatCardModule, 
-    MatCheckboxModule, 
-    FormsModule, 
-    MatRadioModule
-    
+    MatCardModule,
+    MatCheckboxModule,
+    FormsModule,
+    MatRadioModule,
+    JsonPipe,
+    FormsModule,
+    ReactiveFormsModule
+
   ],
   templateUrl: './reasons-why-segregated-fund.component.html',
   styleUrls: ['./reasons-why-segregated-fund.component.css']
@@ -60,28 +68,48 @@ export class ReasonsWhySegregatedFundComponent implements OnInit {
   users: User[] = [];
   selectedUser: string | undefined;
   carrier: Carrier[] = [];
-  selectedCarrier: string | undefined; 
+  selectedCarrier: string | undefined;
   checked = false;
   indeterminate = false;
-  disabled = false
+  disabled = false;
+  salescharge: SalesCharge[] = [];
+  amount: Amounts[] = [];
+  amountSelect: string | undefined;
 
-  constructor(private http: HttpClient, private ServicesService: ServicesService) { }
+  Investments = this._formBuilder.group({
+    savings: false,
+    estateplanning: false,
+    retirement: false,
+    education: false
+  });
+
+  constructor(private http: HttpClient, private ServicesService: ServicesService, private _formBuilder: FormBuilder) { }
   currentDate = new Date();
   currentDateTime = new FormControl(this.currentDate);
   currencyControl = new FormControl('', [Validators.required]);
 
   ngOnInit(): void {
 
-   this.ServicesService.getUserInfo().subscribe((data: any) =>{
-    this.users = data;
-    console.log('Users data:', this.users);
-    
-   }),
+    this.ServicesService.getUserInfo().subscribe((data: any) => {
+      this.users = data;
+      //console.log('Users data:', this.users);
 
-   this.ServicesService.getCarrier().subscribe((carrierData: any) => {
-    this.carrier = carrierData;
-    console.log('Carrier data', this.carrier);
-   })
+    }),
+
+      this.ServicesService.getCarrier().subscribe((carrierData: any) => {
+        this.carrier = carrierData;
+        //console.log('Carrier data', this.carrier);
+      }),
+
+      this.ServicesService.getSalesCharge().subscribe((salesChargeData: any) => {
+        this.salescharge = salesChargeData;
+      })
+
+    this.ServicesService.getAmounts().subscribe((amountData: any) => {
+      console.log(amountData);
+      this.amount = amountData;
+    })
+
   }
 
 }
--- src/app/services/services.service.ts
+++ src/app/services/services.service.ts
@@ -10,16 +10,25 @@ import { Observable } from 'rxjs';
 export class ServicesService {
 
   userinfo: any = './json/users.json';
-  carrier : any = './json/carriers.json';
+  carrier: any = './json/carriers.json';
+  salescharge: any = './json/salescharge.json';
+  amount: any = './json/amounts.json';
 
   constructor(private http: HttpClient) { }
 
   getUserInfo() {
     return this.http.get(this.userinfo);
   }
- 
-  getCarrier(){
-    return this.http.get(this.carrier)
+
+  getCarrier() {
+    return this.http.get(this.carrier);
   }
 
+  getSalesCharge() {
+    return this.http.get(this.salescharge);
+  }
+
+  getAmounts() {
+    return this.http.get(this.amount);
+  }
 }
